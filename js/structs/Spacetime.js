029d97d5f9cb9d0192673c05791844589a709cc8
Alex <climie.alex@gmail.com>
2024-12-27 12:15:47 +0800

    warping caching

--- js/structs/Spacetime.js
+++ js/structs/Spacetime.js
@@ -1,17 +1,12 @@
 export class Spacetime {
     constructor(app) {
         this.app = app;
-        this.curves = [];
         this.density = 40; // smaller = more accurate
+        this.notFirst = 0
+        this.lines = new Map();
     }
 
     update(objects, local) {
-        this.curves.forEach((curve) => {
-            curve.clear();
-        });
-
-        this.curves = [];
-
         this.drawLines(local[0] % this.density - 100, this.density, local[1] % this.density - 100, this.density, objects);
     }
 
@@ -28,7 +23,7 @@ export class Spacetime {
                     let dy = y - object.ref.y;
 
                     let distSquared = Math.max(dx ** 2 + dy ** 2, 0.001);
-                    let force = (object.mass * 300) / distSquared;
+                    let force = (object.mass * 1000) / distSquared;
 
                     let deltaForces = [dx / distSquared * force, dy / distSquared * force];
 
@@ -49,26 +44,49 @@ export class Spacetime {
         for (let i = 0; i < allPoints.length; i++) {
             for (let j = 0; j < allPoints[i].length; j++) {
                 let line;
-                if (false && this.curves.length > 0) {
-                    line = this.curves.shift();
-                    line.clear()
-                } 
-                
-                else {
-                    line = new PIXI.Graphics();
-                    line.alpha = 0.2;
-                    this.app.stage.addChild(line);
+
+                if (j != 0) {
+                    const key = `${i},${j-1},${i},${j}`;
+
+                    if (!this.lines.has(key)) {
+                        line = new PIXI.Graphics();
+                        line.alpha = 0.2;
+                        this.app.stage.addChild(line);
+
+                        this.lines.set(key, line);
+                    }
+                    else {
+                        line = this.lines.get(key);
+                    }
+
+                    this.drawSingleLine(line, allPoints[i][j - 1], allPoints[i][j]);
                 }
+                if (i != 0) {
+                    const key = `${i-1},${j},${i},${j}`;
 
-                if (j != 0) this.drawSingleLine(line, allPoints[i][j - 1], allPoints[i][j]);
-                if (i != 0) this.drawSingleLine(line, allPoints[i - 1][j], allPoints[i][j]);
+                    if (!this.lines.has(key)) {
+                        console.log('no cache...')
+                        line = new PIXI.Graphics();
+                        line.alpha = 0.2;
+                        this.app.stage.addChild(line);
 
-                this.curves.push(line);
+                        this.lines.set(key, line);
+                    }
+                    else {
+                        console.log('cached!')
+                        line = this.lines.get(key);
+                    }
+
+                    this.drawSingleLine(line, allPoints[i - 1][j], allPoints[i][j]);
+                }
             }
         }
+
+        this.notFirst = 1;
     }
 
     drawSingleLine(line, start, end) {
+        line.clear();
         line.moveTo(start[0], start[1]);
         line.lineTo(end[0], end[1]);
         line.stroke({width: 1, color: 0x82cbff});
