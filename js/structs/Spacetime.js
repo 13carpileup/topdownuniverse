0d61b6f6f7619d1b97fa6d5e04dc4ef7ff12dd95
Alex <climie.alex@gmail.com>
2024-12-27 11:42:10 +0800

    refactor

--- js/structs/Spacetime.js
+++ js/structs/Spacetime.js
@@ -7,23 +7,18 @@ export class Spacetime {
 
     update(objects, local) {
         this.curves.forEach((curve) => {
-            curve.destroy();
-        })
+            curve.clear();
+        });
 
-
-        this.curves = []
-        
-        this.drawLines(local[0] % this.density, this.density, local[1] % this.density, this.density, objects);
-   
-        
+        this.drawLines(local[0] % this.density - 100, this.density, local[1] % this.density - 100, this.density, objects);
     }
 
-    drawLines(xBegin, xDelta, yBegin, yDelta, objects, rev = 0) {
-        let allPoints = []
-        for (let x = xBegin; x < this.app.screen.width; x += xDelta) {
-            let points = []
+    drawLines(xBegin, xDelta, yBegin, yDelta, objects) {
+        let allPoints = [];
+        for (let x = xBegin; x < this.app.screen.width + 100; x += xDelta) {
+            let points = [];
 
-            for (let y = yBegin; y < this.app.screen.height; y += yDelta) {
+            for (let y = yBegin; y < this.app.screen.height + 100; y += yDelta) {
                 let forces = [0, 0];
 
                 objects.forEach((object) => {
@@ -37,12 +32,11 @@ export class Spacetime {
 
                     if (Math.abs(deltaForces[0]) > Math.abs(dx)) deltaForces[0] = dx;
                     if (Math.abs(deltaForces[1]) > Math.abs(dy)) deltaForces[1] = dy;
-                    
+
                     forces[0] += deltaForces[0];
                     forces[1] += deltaForces[1];
-                }) 
-                
-                
+                });
+
                 let transformed = [x - forces[0], y - forces[1]];
                 points.push(transformed);
             }
@@ -52,24 +46,29 @@ export class Spacetime {
 
         for (let i = 0; i < allPoints.length; i++) {
             for (let j = 0; j < allPoints[i].length; j++) {
-                if (j!=0) this.drawSingleLine(allPoints[i][j-1], allPoints[i][j]);
-                if (i!=0) this.drawSingleLine(allPoints[i-1][j], allPoints[i][j]);
+                let line;
+                if (false && this.curves.length > 0) {
+                    line = this.curves.shift();
+                    line.clear()
+                } 
+                
+                else {
+                    line = new PIXI.Graphics();
+                    line.alpha = 0.2;
+                    this.app.stage.addChild(line);
+                }
+
+                if (j != 0) this.drawSingleLine(line, allPoints[i][j - 1], allPoints[i][j]);
+                if (i != 0) this.drawSingleLine(line, allPoints[i - 1][j], allPoints[i][j]);
+
+                this.curves.push(line);
             }
         }
     }
 
-    drawSingleLine(start, end) {
-        let line = new PIXI.Graphics();
-        
-
-        line.position.set(start[0], start[1]);
-
-        line.moveTo(0, 0)
-        line.lineTo(end[0] - start[0], end[1] - start[1]);
-        line.stroke({ width: 1, color: '0x82cbff' });
-        line.alpha = 0.2;
-        
-        this.curves.push(line);
-        this.app.stage.addChild(line);
+    drawSingleLine(line, start, end) {
+        line.moveTo(start[0], start[1]);
+        line.lineTo(end[0], end[1]);
+        line.stroke({width: 1, color: 0x82cbff});
     }
- }
+}
