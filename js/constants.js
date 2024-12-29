3738d2113e3433567cb7b2c7b45f942e2ff273a1
Alex <climie.alex@gmail.com>
2024-12-29 16:34:17 +0800

    constants file

---
+++ js/constants.js
@@ -0,0 +1,9 @@
+export const constants = {
+    // gravity
+    gravitationalConstant: (1/2),
+    flingAmplification: 8,
+
+    // trails
+    trailOn: 3,
+    trailOff: 7,
+}
--- js/structs/Object.js
+++ js/structs/Object.js
@@ -1,4 +1,4 @@
-import { constants } from "../constants";
+import { constants } from "../constants.js";
 
 export class Object {
     constructor(ref, radius, x, y, velocity, angle, mass) {
